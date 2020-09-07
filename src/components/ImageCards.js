23d48c65e61d5dde5d6d8cfb9395fa9c96ea23c3
Jeremy Roussel <jeremy.roussel13@gmail.com>
2020-09-07 14:27:37 -0500

    Image Upload Feature Complete

--- src/components/ImageCards.js
+++ src/components/ImageCards.js
@@ -4,21 +4,15 @@ import React, {useEffect, useState} from 'react'
 import SampleImage from './SampleImage'
 
 // Route Hooks
-// import { useRouteMatch, useHistory} from 'react-router-dom'
 import { useRouteMatch } from 'react-router-dom'
-
-// Redux Hooks
-// import {useSelector, useDispatch} from 'react-redux'
-
-// Action Creators
-
 
 // Material-UI Components and Hook
 import {makeStyles} from '@material-ui/styles'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-import { DropzoneArea } from 'material-ui-dropzone'
+import { DropzoneAreaBase } from 'material-ui-dropzone'
+import Button from '@material-ui/core/Button'
 
 // Initialize Sample Information
 const initImages = []
@@ -37,6 +31,10 @@ const useStyles = makeStyles((theme) => ({
         overflow: 'auto',
         flexDirection: 'column',
     },
+    buttonStyle: {
+        marginTop: theme.spacing(2),
+        width: '25%'
+    }
 
   }));
 
@@ -45,22 +43,16 @@ function ImageCards() {
     // invoke Style Hook
     const classes = useStyles();
 
-    // Use History to allow navigation when sending a new sample
-    // let history = useHistory();
-
-    // Create state for component
+     // Create state for component
     const [isLoading, setIsLoading] = useState(true)
     const [isError, setIsError] = useState(true)
     const [sampleImages, setSampleImages] = useState(initImages)
-
-
-    // Define dispatch
-    // const dispatch = useDispatch()
+    const [fileObject, setFileObject] = useState([])
+    const [refresh, setRefresh] = useState(false)
 
     // Identify current sample
     let matchID = useRouteMatch("/sample/:id").params.id;
 
-   
     // Create Array of JSX Card Elemnts of the images
     const imgElementArray = sampleImages.map(item => {
         return  <Grid item xs={12} md={4} lg={3} key={item.id}> <SampleImage imageItem={item} /> </Grid>
@@ -86,23 +78,29 @@ function ImageCards() {
             setIsLoading(false);
         }
         fetchData();
-      }}, [matchID])
+      }}, [matchID, refresh])
 
     // Upload Function
-    // const fileAdded = () => {
-    //     const fetchData = async () => {
-    //         let formData = new FormData()
-    //         formData.append('image', file)
-    //         let response = await fetch('http://localhost:3001/upload', {
-    //             method: "POST",
-    //             body: {formData}
-    //         })
-    //         let result = await response.json()
-    //         console.log(result)
-    //         history.push("/sample/"+matchID)
-    //     }
-    //     fetchData()
-    // }
+    const fileAdded = async () => {
+
+        var formData = new FormData()
+        formData.append('image', fileObject[0].file, fileObject[0].file.name)
+        formData.append('sampleID', matchID)
+
+        try {
+            let response = await fetch('http://localhost:3001/uploadTS', {
+                method: 'POST',
+                body: formData
+            })
+            let success = await response.json()
+
+            console.log('success', success)
+            setRefresh(!refresh)
+            setFileObject([])
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     // conditional render for loading, which should be replaced by the images in all cases execpt 'add'
     return (isLoading && isError && (matchID === 'add'))? (
@@ -119,24 +117,34 @@ function ImageCards() {
                 </Paper>
             </Grid>
 
-            {imgElementArray}
+            {imgElementArray}       
 
-            {/* Image Upload Element */}
-        
-            <Grid item xs={12} >
+            <Grid item xs={12} lg={6} key="Add Image">
                 <Paper className={classes.paper} >
-                    <Typography variant="body1" component="h2" align="center" >
-                        <DropzoneArea
-                            acceptedFiles={['image/*']}
-                            dropzoneText={"Drag and drop an image here or click"}
-                            maxFileSize={30000000}
-                            filesLimit={1}
-                            onDrop={(file) => {
-                                console.log(`file added:`, file)
-                                // fileAdded(file)
-                            }}
-                        />
-                    </Typography>
+                    
+                    <DropzoneAreaBase
+                        acceptedFiles={['image/*']}
+                        fileObjects={fileObject}
+                        maxFileSize={30000000}
+                        filesLimit={1}
+                        onAdd={(file) => {
+                            // console.log(`on Add:`, file)
+                            setFileObject([].concat(fileObject, file))
+                        }}
+                        onDelete={file => {
+                            // console.log(`on Delete:`, file)
+                            setFileObject([])
+                        }}
+                        dropzoneText={"Drag and drop an image here or click"}
+                    />
+                    <Button variant="contained" color="primary" className="buttonStyle"
+                    onClick={() => {
+                        // console.log('Upload')
+                        fileAdded()}}
+                    >
+                        Add Image
+                    </Button>
+
                 </Paper>
             </Grid>
             
